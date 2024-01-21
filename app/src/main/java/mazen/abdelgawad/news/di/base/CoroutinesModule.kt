5dca5f998dbc8da1c09b7e88139b30664039027b
Mazen Abdelgawad <eng.mazen96@gmail.com>
2024-01-21 03:09:42 +0200

    Add CoroutinesModle di

---
+++ app/src/main/java/mazen/abdelgawad/news/di/base/CoroutinesModule.kt
@@ -0,0 +1,22 @@
+package mazen.abdelgawad.news.di.base
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CoroutinesModule {
+
+    @Provides
+    @IoDispatcher
+    fun providesIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @Provides
+    @MainDispatcher
+    fun providesMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
+}

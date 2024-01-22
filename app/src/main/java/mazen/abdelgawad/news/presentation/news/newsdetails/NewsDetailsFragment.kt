91a05480cbd864a623ab6f4e537f5bf4b461525c
Mazen <eng.mazen96@gmail.com>
2024-01-22 02:34:34 +0200

    handle show new details screen

--- app/src/main/java/mazen/abdelgawad/news/presentation/news/newsdetails/NewsDetailsFragment.kt
+++ app/src/main/java/mazen/abdelgawad/news/presentation/news/newsdetails/NewsDetailsFragment.kt
@@ -6,7 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import mazen.abdelgawad.news.R
 import mazen.abdelgawad.news.databinding.FragmentNewsDetailsBinding
+import mazen.abdelgawad.news.domain.modle.News
 
 class NewsDetailsFragment : Fragment() {
     private val binding by lazy { FragmentNewsDetailsBinding.inflate(layoutInflater) }
@@ -14,11 +18,32 @@ class NewsDetailsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
-        val news = args.selectedNews
+        this.shwNewsDetails(args.selectedNews)
 
-        binding.zaas.text = news.title
         return binding.root
     }
+
+    fun shwNewsDetails(item: News) = with(binding) {
+        title.text = item.title ?: ""
+        author.text = item.author ?: ""
+        publishedAt.text = item.publishedAt ?: ""
+        description.text = item.description
+        url.text = item.url ?: "https://www.google.com/search?q=$title"
+
+        if (item.urlToImage.isNullOrBlank()) {
+            imageView.visibility = View.GONE
+        } else {
+            imageView.visibility = View.VISIBLE
+            Glide
+                .with(this@NewsDetailsFragment)
+                .load(item.urlToImage)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .centerCrop()
+                .placeholder(R.drawable.placeholder_img)
+                .into(imageView)
+        }
+    }
+
 }

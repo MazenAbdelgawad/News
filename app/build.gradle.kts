ee04d70f1e3c1ed4e16fbb33909001e3a6169678
Mazen <eng.mazen96@gmail.com>
2024-01-22 09:37:01 +0200

    release-v1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "mazen.abdelgawad.news"
         minSdk = 21
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
